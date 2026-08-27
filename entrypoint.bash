d3b4a97945632e61445693227942e2ad45700bd9
k-shenbagaraj <shenbax@seas.upenn.edu>
2026-08-27 14:28:11 -0400

    updated entrypoint to match new launch files

--- entrypoint.bash
+++ entrypoint.bash
@@ -10,13 +10,13 @@ source /opt/ros/jazzy/setup.bash
 source ws/install/setup.bash
 if [ "${RTK}" == "true" ]; then
     if [[ " ${UGVS[*]} " == *" $HOSTNAME "* ]]; then
-        ros2 launch rtk_correction receiver.launch.py ip:=$RTK_IP port:=$RTK_PORT
+        ros2 launch rtk_correction receiver.launch.py
     
     elif [[ " ${UAVS[*]} " == *" $HOSTNAME "* ]]; then
-        ros2 launch rtk_correction receiver.launch.py ip:=$RTK_IP port:=$RTK_PORT
+        ros2 launch rtk_correction receiver.launch.py
     
     elif [[ " ${BASESTATIONS[*]} " == *" $HOSTNAME "* ]]; then
-        ros2 launch rtk_correction broadcaster.launch.py ip:=$RTK_IP port:=$RTK_PORT 
+        ros2 launch rtk_correction broadcaster.launch.py 
     else
         echo "Error: Hostname '$HOSTNAME' not recognized."
         exit 1
